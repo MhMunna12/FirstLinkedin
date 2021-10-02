1d2828262cc7d3c1957f86c5dc282e48fda2a5c2
Monwar Hossain <monwarhossainmunna@gmail.com>
2021-10-03 02:21:56 +0600

    first

---
+++ src/component/AllHome/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            This Is Home
+            <p>Woow Beautiful</p>
+        </div>
+    );
+};
+
+export default Home;
